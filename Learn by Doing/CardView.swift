c4df999ec42aaea9255fb07e52545757efb5b79f
hyesungpark <jayhspark@gmail.com>
2023-11-27 22:37:45 +0900

    add alert

--- Learn by Doing/CardView.swift
+++ Learn by Doing/CardView.swift
@@ -14,6 +14,7 @@ struct CardView: View {
   @State private var fadeIn: Bool = false
   @State private var moveDownward: Bool = false
   @State private var moveUpward: Bool = false
+  @State private var showAlert: Bool = false
   
   var gradient: [Color] = [Color("Color01"), Color("Color02")]
   
@@ -39,6 +40,7 @@ struct CardView: View {
       Button(action: {
         print("Button was tapped")
         playSound(sound: "sound-chime", type: "mp3")
+        self.showAlert.toggle()
       }, label: {
         HStack {
           Text(card.callToAction.uppercased())
@@ -83,6 +85,13 @@ struct CardView: View {
         self.moveUpward.toggle()
       }
     }
+    .alert(isPresented: $showAlert, content: {
+      Alert(
+        title: Text(card.title),
+        message: Text(card.message),
+        dismissButton: .default(Text("OK"))
+      )
+    })
   }
 }
 
