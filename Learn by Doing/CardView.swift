a2934eeff84f77065b95b92d1420413cd667fd00
hyesungpark <jayhspark@gmail.com>
2023-11-27 22:07:53 +0900

    finish card view

--- Learn by Doing/CardView.swift
+++ Learn by Doing/CardView.swift
@@ -16,6 +16,46 @@ struct CardView: View {
   var body: some View {
     ZStack {
       Image("developer-no1")
+      
+      VStack {
+        Text("SwfitUI")
+          .font(.largeTitle)
+          .fontWeight(.heavy)
+          .multilineTextAlignment(.center)
+          .foregroundStyle(Color.white)
+        Text("Better apps. Less code.")
+          .fontWeight(.light)
+          .foregroundColor(Color.white)
+          .italic()
+      }
+      .offset(y: -218)
+      
+      Button(action: {
+        print("Button was tapped")
+      }, label: {
+        HStack {
+          Text("Learn".uppercased())
+            .fontWeight(.heavy)
+            .foregroundStyle(Color.white)
+            .accentColor(Color.white)
+          
+          Image(systemName: "arrow.right.circle")
+            .font(Font.title.weight(.medium))
+            .accentColor(Color.white)
+          
+        }
+        .padding(.vertical)
+        .padding(.horizontal, 24)
+        .background(
+          LinearGradient(
+            gradient: Gradient(colors: gradient),
+            startPoint: .leading,
+            endPoint: .trailing)
+        )
+        .clipShape(Capsule())
+        .shadow(color: Color("ColorShadow"), radius: 6, x: 0, y: 3)
+      })
+      .offset(y: 210)
     }
     .frame(width: 335, height: 545)
     .background(
